65516661bcf1ab074faefcbd0cdf7baa6ef24cac
DongGyuLee <dlehdrb1129@gmail.com>
2020-09-18 21:13:54 +0900

    20200918 R selenium study

---
+++ naverhotel.R
@@ -0,0 +1,37 @@
+library(RSelenium) #해결 못함....
+remDr <- remoteDriver(remoteServerAddr = "localhost" , port = 4445, browserName = "chrome")
+remDr$open()
+url<-'https://hotel.naver.com/hotels/item?hotelId=hotel:Shilla_Stay_Yeoksam&destination_kor=%EC%8B%A0%EB%9D%BC%EC%8A%A4%ED%85%8C%EC%9D%B4%20%EC%97%AD%EC%82%BC&rooms=2'
+remDr$navigate(url)
+Sys.sleep(3)
+pageLink <- NULL
+reple <- NULL
+curr_PageOldNum <- 0
+#curr_PageNewNum <- curr_PageOldNum + 1
+repeat{
+  doms <- remDr$findElements(using = "css selector", "div.review_desc > p")
+  Sys.sleep(1)
+  reple_v <- sapply(doms, function (x) {x$getElementText()})
+  print(reple_v)
+  reple <- append(reple, unlist(reple_v))
+  cat(length(reple), "\n")
+  pageLink <- remDr$findElements(using='css',".hotel_used_review.ng-isolate-scope > div.review_ta.ng-scope > div.paginate > a.direction.next")
+  remDr$executeScript("arguments[0].click();",pageLink)
+  Sys.sleep(2)
+  
+  #body > div > div.ng-scope > div.container.ng-scope > div.content > div.hotel_used_review.ng-isolate-scope > div.review_ta.ng-scope > div.paginate > a.direction.next.disabled
+  #body > div > div.ng-scope > div.container.ng-scope > div.content > div.hotel_used_review.ng-isolate-scope > div.review_ta.ng-scope > div.paginate > span:nth-child(4) > a
+  #body > div > div.ng-scope > div.container.ng-scope > div.content > div.hotel_used_review.ng-isolate-scope > div.review_ta.ng-scope > div.paginate > span:nth-child(2) > strong
+  curr_PageElem <- remDr$findElement(using='css','.hotel_used_review.ng-isolate-scope > div.review_ta.ng-scope > div.paginate > span > .ng-binding')
+  curr_PageNewNum <- as.numeric(curr_PageElem$getElementText())
+  cat(paste(curr_PageOldNum, ':', curr_PageNewNum,'\n'))
+  if(curr_PageNewNum == curr_PageOldNum)  {
+    cat("종료\n")
+    break; 
+  }
+  curr_PageOldNum <- curr_PageNewNum;
+}
+cat(length(reple), "개의 댓글 추출\n")
+write(reple,"naverhotel.txt")
+
+body > div > div.ng-scope > div.container.ng-scope > div.content > div.hotel_used_review.ng-isolate-scope > div.review_ta.ng-scope > div.paginate > a.direction.next.disabled
