82aa6161f1c7290a50b3d813038e1232f2c46e03
DongGyuLee <dlehdrb1129@gmail.com>
2020-09-29 18:21:36 +0900

    20200929 R selenium project

--- 20200928_s_project.R
+++ 20200928_s_project.R
@@ -7,65 +7,44 @@ remDr$open()
 site <- 'https://www.acmicpc.net/problem/tags'
 remDr$navigate(site)
 
-#problem_title <- NULL
-#problem_submit <- NULL
-pageLink <- NULL
-algo_title <- NULL
-problem_num <- NULL
+problem_title <- NULL
+problem_submit <- NULL
 answer_percent <- NULL
+  pageLink <- NULL
 
-for (n in 98:99) {
-answer_percent <- NULL
-  #태그로 이동_문제수가 100 넘어가는 것들만 1~30번까지
+for (n in 1:30) {
+  #태그로 이동_문제수가 100 넘어가는 것들만 1~30번까지지
   Sys.sleep(5)
   pageLink <- remDr$findElements(using='xpath',
                                  value= paste0('/html/body/div[3]/div[2]/div[5]/div/div/table/tbody/tr[', n, ']/td[1]/a'))
-  
-  #알고리즘 태그명
-  algo_titles <- sapply(pageLink, function(x) {x$getElementText()})
-  print(algo_titles)
-  algo_title <- append(algo_title, unlist(algo_titles))
-  
-  
-  #태그별 문제수
-  algo_node <- remDr$findElements(using='xpath',
-                                 value= paste0('/html/body/div[3]/div[2]/div[5]/div/div/table/tbody/tr[', n, ']/td[3]'))
-  problem_nums <- sapply(algo_node, function(x) {x$getElementText()})
-  print(problem_nums)
-  problem_num <- append(problem_num, unlist(problem_nums))
-  
-  
-  #태그 클릭
   remDr$executeScript("arguments[0].click();",pageLink)
   
-  Sys.sleep(3)
+  Sys.sleep(1)
   pageLink_next <- NULL
   curr_PageOldNum <- 0
-  
   repeat{
     #문제 제목
-    #problem_nodes <- remDr$findElements(using='css', '#problemset > tbody > tr > td:nth-child(2)')
-    #problem_titles <- sapply(problem_nodes, function(x) {x$getElementText()})
-    #print(problem_titles)
-    #problem_title <- append(problem_title, unlist(problem_titles))
+    problem_nodes <- remDr$findElements(using='css', '#problemset > tbody > tr > td:nth-child(2)')
+    problem_titles <- sapply(problem_nodes, function(x) {x$getElementText()})
+    print(problem_titles)
+    problem_title <- append(problem_title, unlist(problem_titles))
     
     #제출한 사람
-    #problem_nodes <- remDr$findElements(using='css', '#problemset > tbody > tr > td:nth-child(5)')
-    #problem_submits <- sapply(problem_nodes, function(x) {x$getElementText()})
-    #print(problem_submits)
-    #problem_submit <- append(problem_submit, unlist(problem_submits))
-    
+    problem_nodes <- remDr$findElements(using='css', '#problemset > tbody > tr > td:nth-child(5)')
+    problem_submits <- sapply(problem_nodes, function(x) {x$getElementText()})
+    print(problem_submits)
+    problem_submit <- append(problem_submit, unlist(problem_submits))
     
     #정답 비율
     problem_nodes <- remDr$findElements(using='css', '#problemset > tbody > tr > td:nth-child(6)')
     answer_percents <- sapply(problem_nodes, function(x) {x$getElementText()})
-    #print(answer_percents)
+    print(answer_percents)
     answer_percent <- append(answer_percent, unlist(answer_percents))
     
     #다음페이지(다음 버튼이 없는데 다음 페이지의 숫자에 자동으로 #next_page가 붙음..좋은 사이트)
     pageLink_next <- remDr$findElements(using='css',"#next_page")
     remDr$executeScript("arguments[0].click();",pageLink_next)
-    Sys.sleep(1)
+    Sys.sleep(2)
     
     curr_PageElem <- remDr$findElement(using='css', 'div.wrapper > div.container.content > div:nth-child(6) > div:nth-child(2) > div > ul > li.active')
     curr_PageNewNum <- as.numeric(curr_PageElem$getElementText())
@@ -75,14 +54,11 @@ answer_percent <- NULL
       #태그 하나 종료 시 다시 처음 화면으로
       site <- 'https://www.acmicpc.net/problem/tags'
       remDr$navigate(site)
-      df <- data.frame(algo_title, problem_num, answer_percent)
-      write.csv(df, 'BEAKJOON.csv')#, append = T)
+      df <- data.frame(problem_title, problem_submit, answer_percent)
+      write.csv(df, 'BEAKJOON.csv', append = T)
       break; 
     }
     curr_PageOldNum <- curr_PageNewNum;
   }
 }
-df <- data.frame(algo_title, problem_num, answer_percent)
 View(df)
-write.csv(df, 'BEAKJOON.csv')
-rm(df)
