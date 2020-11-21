07a3e2d2d3c1ceaa7d3b6fc513f23e1bc9386c59
DongGyuLee <dlehdrb1129@gmail.com>
2020-11-21 19:30:35 +0900

    20201121_융복합프로젝트_요기요크롤링

---
+++ yogiyo.R
@@ -0,0 +1,25 @@
+#cmd -> d: -> cd D:\R기반_빅데이터처리\Rexam
+# -> java -Dwebdriver.chrome.driver="chromedriver.exe" -jar selenium-server-standalone-4.0.0-alpha-1.jar -port 4445
+
+library(RSelenium)
+remDr <- remoteDriver(remoteServerAddr = "localhost" , 
+                      port = 4445, browserName = "chrome")
+
+#요기요_셰플리-셰프의건강한한끼(강남)
+remDr$open()
+site <- 'https://www.yogiyo.co.kr/mobile/#/260443/'
+remDr$navigate(site)
+
+Sys.sleep(3)
+
+#클린리뷰 탭 클릭
+pageLink <- remDr$findElements(using='css',
+                               value= paste0('#content > div.restaurant-detail.row.ng-scope > div.col-sm-8 > ul > li:nth-child(2) > a'))
+remDr$executeScript("arguments[0].click();",pageLink)
+
+#작업 디렉토리에 새폴더 생성(csv파일 저장 폴더)
+#dir.create('yogiyo')
+
+for (n in 2:11) {
+  
+}
